3475931259fc4513daa9a2c6cdbca92b3e815929
Subham Sahu <subham7sahu@gmail.com>
2023-03-04 19:52:47 +0100

    feat: restrict view to only smaller screens

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,13 +3,14 @@ import { AUTH_STATUS_API } from '@/utils/config';
 import { useAuthStore } from '@/utils/useAuthStore';
 import axios from 'axios';
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   const { setUser } = useAuthStore()
   useEffect(() => {
+
     axios.get(AUTH_STATUS_API).then(e => {
       if (e.data.is_authenticated) {
         setUser(e.data.user, false)
@@ -21,8 +22,20 @@ export default function App({ Component, pageProps }: AppProps) {
       setUser(null, false, err.message)
     })
   }, [])
+
+
+  const [isWidthOkay,setIsWidthOkay]=useState(true)
+
+  useEffect(() => {
+    console.log(window.innerWidth)
+    const handleEvent = (ev: any) => {
+      setIsWidthOkay(window.innerWidth<450)
+    }
+    window.addEventListener('resize', handleEvent)
+    return () => window.removeEventListener('resize', handleEvent)
+  }, [])
   return <>
-    <Component {...pageProps} />
+    {isWidthOkay? <Component {...pageProps} />: <div className='text-center mt-10 font-bold'>Sorry! this webapp is only for mobile phones. 😞</div>}
     <ToastContainer autoClose={2000} />
   </>
 }
